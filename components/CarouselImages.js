1dfd1356267441786243012700976991852e2b75
David Fernando Santa <santadavid88@hotmail.com>
2021-04-24 10:01:01 -0500

    Coursel Images  Paginations
    Coursel Images  Paginations

--- components/CarouselImages.js
+++ components/CarouselImages.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {ActivityIndicator,StyleSheet, Text, View } from 'react-native'
-import Carousel from 'react-native-snap-carousel'
 import {Image} from 'react-native-elements'
+import Carousel,{Pagination} from 'react-native-snap-carousel'
+import {size} from 'lodash'
 
-export default function CarouselImages({images,height, width}) {
+export default function CarouselImages({images,height, width,activeSlide,setActiveSlide}) {
   
     const renderItem=({item})=>{
       return(
@@ -14,16 +15,59 @@ export default function CarouselImages({images,height, width}) {
          />
       )
     }
-    return (       
-        <Carousel
+    return ( 
+   <View>
+       <Carousel
           layout={"default"}
           data={images}
           sliderWidth={width}
           itemWidth={width}
           itemHeight={height}
           renderItem={renderItem}
+          onSnapToItem={(index)=>setActiveSlide(index)}
         />
+        <MyPagintation data={images} activeSlide={activeSlide}/>
+     </View>      
+      
     )
 }
 
-const styles = StyleSheet.create({})
+
+function MyPagintation({data,activeSlide}){
+  return(
+    <Pagination
+       dotsLength={size(data)}
+       activeDotIndex={activeSlide}
+       containerStyle={styles.containerPagination}
+       dotStyle={styles.dotActive}
+       inactiveDotStyle={styles.dotInactive}
+       inactiveDotOpacity={0.6}
+       inactiveDotScale={0.6}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+   containerPagination:{
+     backgroundColor:"transparent",
+     zIndex:1,
+     position:"absolute",
+     bottom:0,
+     alignSelf:"center"
+   },
+   dotActive:{
+     width:20,
+     height: 20,
+     borderRadius:10,
+     marginHorizontal:2,
+     backgroundColor:"#442484"
+   },
+   dotInactive:{
+    width:14,
+    height: 14,
+    borderRadius:7,
+    marginHorizontal:2,
+    backgroundColor:"#fff"
+  }
+
+})
